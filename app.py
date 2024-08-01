5728ce468608f8217d9643425aa0185b0b4133f3
Nihileswaran M.P <123666501+nihileswaran@users.noreply.github.com>
2024-08-01 19:53:46 +0000

    2 update

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
-app=Flask(__name__)
+
+app = Flask(__name__,template_folder='template')
+
+JOBS = [
+    {
+    "id": 1,
+    "title": "developer",
+    "salary": 50000,
+    "location": "Banglore"
+    },
+    {
+    "id": 2,
+    "title": "Frontend developer",
+    
+    "location": "Chennai"
+    }]
+
 
 @app.route("/")
 def hello_world():
-  return "hello Nihil"
+  return render_template('home.html',jobs=JOBS,company='chools')
+
+@app.route("/jobs")
+def job():
+  return jsonify(JOBS)
+  
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
-
+  app.run(host='0.0.0.0', debug=True)
